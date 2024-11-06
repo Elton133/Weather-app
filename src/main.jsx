a39ae439be82580a006e34411372b00ae95e061d
Elton133 <134627073+Elton133@users.noreply.github.com>
2024-11-06 19:29:01 +0000

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from './App.jsx'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-     navigator.serviceWorker.register('/service-worker.js').then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
-     }).catch(error => {
-        console.log('Service Worker registration failed:', error);
-     });
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful');
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
   });
 }
 
